2129a87300017b996c4b77e4d48e25c9e3cd8e36
soggy6 <soggy6@users.noreply.github.com>
2015-12-10 17:07:10 -0500

    silly mistake

--- helloworld.php
+++ helloworld.php
@@ -1,6 +1,6 @@
 <?php
   echo 'Hello World!<br/>';
   echo date('l \t\h\e jS') . '<br/>';
-  echo 'My username is ' getenv('JOHN');. '!';
+  echo 'My username is ' getenv('JOHN') . '!';
   echo '<br/> random change for no reason';
 ?>
