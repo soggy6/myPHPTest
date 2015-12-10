277c8e8e3d3f2d98f87503057bde39170475efaa
soggy6 <soggy6@users.noreply.github.com>
2015-12-10 17:10:17 -0500

    more silly

--- helloworld.php
+++ helloworld.php
@@ -1,6 +1,7 @@
 <?php
+  $myname = getenv('REMOTE_ADDR');
   echo 'Hello World!<br/>';
   echo date('l \t\h\e jS') . '<br/>';
-  echo 'My username is ' getenv('JOHN') . '!';
+  echo 'My username is ' $myname . '!';
   echo '<br/> random change for no reason';
 ?>
