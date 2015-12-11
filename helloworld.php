ea7d87f2a6ac422407e49ac8a23089bc350260ab
soggy6 <soggy6@users.noreply.github.com>
2015-12-11 09:33:28 -0500

    another tweak

--- helloworld.php
+++ helloworld.php
@@ -2,6 +2,6 @@
   $myname = getenv('JOHN');
   echo 'Hello World!<br/>';
   echo date('l \t\h\e jS') . '<br/>';
-  echo 'My username is ' $myname . '!';
+  echo 'My username is' . $myname;
   echo '<br/> random change for no reason';
 ?>
